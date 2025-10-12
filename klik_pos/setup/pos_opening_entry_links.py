002b4d19bd4fb494a512293c4f54e74d6edd84a9
maniamartial <martialmania19@gmail.com>
2025-10-12 13:07:58 +0300

    chore: precommit

--- klik_pos/setup/pos_opening_entry_links.py
+++ klik_pos/setup/pos_opening_entry_links.py
@@ -1,5 +1,6 @@
 import frappe
 
+
 def ensure_pos_opening_entry_links():
 	"""Ensure required Document Links exist in the POS Opening Entry DocType."""
 	required_links = [
@@ -8,24 +9,24 @@ def ensure_pos_opening_entry_links():
 
 	# Fetch existing links
 	existing_links = frappe.get_all(
-		"DocType Link",
-		filters={"parent": "POS Opening Entry"},
-		fields=["link_doctype", "link_fieldname"]
+		"DocType Link", filters={"parent": "POS Opening Entry"}, fields=["link_doctype", "link_fieldname"]
 	)
 
 	existing_links_set = {(link["link_doctype"], link["link_fieldname"]) for link in existing_links}
 
 	for link in required_links:
 		if (link["link_doctype"], link["link_fieldname"]) not in existing_links_set:
-			doc = frappe.get_doc({
-				"doctype": "DocType Link",
-				"parent": "POS Opening Entry",
-				"parentfield": "links",
-				"parenttype": "DocType",
-				"link_doctype": link["link_doctype"],
-				"link_fieldname": link["link_fieldname"],
-				"group": "POS"
-			})
+			doc = frappe.get_doc(
+				{
+					"doctype": "DocType Link",
+					"parent": "POS Opening Entry",
+					"parentfield": "links",
+					"parenttype": "DocType",
+					"link_doctype": link["link_doctype"],
+					"link_fieldname": link["link_fieldname"],
+					"group": "POS",
+				}
+			)
 			doc.insert(ignore_permissions=True)
 			frappe.db.commit()
 			frappe.msgprint(f"Added missing Document Link: {link['link_doctype']} to POS Opening Entry")
